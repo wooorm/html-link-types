2be8b49d31f448e48371da312578a86adb3402c9
Titus Wormer <tituswormer@gmail.com>
2022-11-16 16:13:05 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,20 +1,16 @@
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {htmlLinkTypes} from './index.js'
 
-test('htmlLinkTypes', function (t) {
-  t.ok(Array.isArray(htmlLinkTypes), 'should be an `array`')
+test('htmlLinkTypes', function () {
+  assert.ok(Array.isArray(htmlLinkTypes), 'should be an `array`')
 
-  t.doesNotThrow(function () {
-    let index = -1
-    while (++index < htmlLinkTypes.length) {
-      assert.equal(
-        typeof htmlLinkTypes[index],
-        'string',
-        '`' + htmlLinkTypes[index] + '` should be string'
-      )
-    }
-  }, 'should be all `string`')
-
-  t.end()
+  let index = -1
+  while (++index < htmlLinkTypes.length) {
+    assert.equal(
+      typeof htmlLinkTypes[index],
+      'string',
+      '`' + htmlLinkTypes[index] + '` should be string'
+    )
+  }
 })
